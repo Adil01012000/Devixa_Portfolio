b57c01d2a06ee8e7268852d3a6938f5df17faf54
Muhammad Adil <hur17728@gmail.com>
2025-09-16 01:02:53 +0500

    Removed banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <>
     <div className="overflow-x-hidden">
-      <Banner />
+      {/* <Banner /> */}
       <Navbar />
       <Hero />
       <LogoTicker />
